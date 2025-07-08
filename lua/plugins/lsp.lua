26ea3ab2b7b8f8903580fb4394c873a33ef83391
DovaPT <peterd5590@gmail.com>
2025-07-09 00:55:01 +0200

    added auto pairs

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -17,5 +17,12 @@ return  {
     sources = {
       default = { 'lsp', 'path', 'snippets', 'buffer' },
     },
+  },
+  {
+    'windwp/nvim-autopairs',
+    event = "InsertEnter",
+    config = true
+    -- use opts = {} for passing setup options
+    -- this is equivalent to setup({}) function
   }
 }
