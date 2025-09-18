823d979ee7fda8048b6e3723f99fde796ba24ca7
DovaPT <peterd5590@gmail.com>
2025-09-19 01:03:45 +0200

    added folds to config

--- init.lua
+++ init.lua
@@ -1,22 +1,28 @@
-vim.o.wrap = false
-
-
---Tab width
+--Tab width and wrap{{{
 vim.o.shiftwidth = 4
 vim.o.softtabstop = 4
 vim.o.expandtab = false
+vim.o.wrap = false
+-- }}}
 
---Clipboard
+--Clipboard{{{
 vim.o.clipboard = "unnamedplus"
+-- }}}
 
---Lazy
+--Folds{{{
+vim.o.foldmethod = 'marker'
+-- }}}
+
+--Lazy{{{
 require("config.lazy")
+-- }}}
 
---Lsp
+--Lsp{{{
 vim.lsp.inlay_hint.enable(true)
 vim.diagnostic.config({ virtual_lines = true, virtual_text = true })
+-- }}}
 
--- Themeing and coloring
+-- Themeing and coloring{{{
 vim.cmd.colorscheme("ayu-dark")
 vim.api.nvim_set_hl(0, 'LineNr', { fg = 'grey', bold = true })
 vim.api.nvim_set_hl(0, 'CursorLineNr', { fg = 'orange', bold = true })
@@ -27,6 +33,8 @@ vim.o.cursorline = true
 vim.o.signcolumn = "yes"
 vim.o.winborder = "single"
 vim.o.colorcolumn = "72"
+-- }}}
 
---keybinds
+--keybinds{{{
 require("keybinds")
+-- }}}
