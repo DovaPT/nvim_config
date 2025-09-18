fdd8a6837a71f21a071e7d7695b7da1f5683295e
DovaPT <peterd5590@gmail.com>
2025-09-19 00:56:29 +0200

    changed layout of config

--- init.lua
+++ init.lua
@@ -1,22 +1,32 @@
-vim.o.number = true
-vim.o.relativenumber = true
 vim.o.wrap = false
-vim.o.expandtab = true
-vim.o.tabstop = 2
-vim.o.shiftwidth = 2
-vim.o.cursorline = true
-vim.o.clipboard = "unnamedplus"
-vim.o.signcolumn = "yes"
-vim.o.winborder = "single"
-vim.o.colorcolumn = "72"
-vim.diagnostic.config({ virtual_lines = true, virtual_text = true })
 
+
+--Tab width
+vim.o.shiftwidth = 4
+vim.o.softtabstop = 4
+vim.o.expandtab = false
+
+--Clipboard
+vim.o.clipboard = "unnamedplus"
+
+--Lazy
 require("config.lazy")
 
+--Lsp
 vim.lsp.inlay_hint.enable(true)
+vim.diagnostic.config({ virtual_lines = true, virtual_text = true })
+
+-- Themeing and coloring
 vim.cmd.colorscheme("ayu-dark")
 vim.api.nvim_set_hl(0, 'LineNr', { fg = 'grey', bold = true })
 vim.api.nvim_set_hl(0, 'CursorLineNr', { fg = 'orange', bold = true })
--- vim.api.nvim_set_hl(0, 'ColorColumn', {bg = 'grey'})
+vim.api.nvim_set_hl(0, 'ColorColumn', { bg = '#606060' })
+vim.o.number = true
+vim.o.relativenumber = true
+vim.o.cursorline = true
+vim.o.signcolumn = "yes"
+vim.o.winborder = "single"
+vim.o.colorcolumn = "72"
 
+--keybinds
 require("keybinds")
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,26 +1,26 @@
-return  {
-  {
-    "mason-org/mason-lspconfig.nvim",
-    opts = {ensure_installed = {}},
-    dependencies = {
-      { "mason-org/mason.nvim", opts = {} },
-      "neovim/nvim-lspconfig",
-    },
-  },
-  {
-    'saghen/blink.cmp',
-    version = '1.*',
-    opts = {
-      keymap = {preset = 'default'},
-      appearance = {nerd_font_variant = 'normal'},
-    },
-    sources = {
-      default = { 'lsp', 'path', 'snippets', 'buffer' },
-    },
-  },
-  {
-    'windwp/nvim-autopairs',
-    event = "InsertEnter",
-    config = true
-  }
+return {
+	{
+		"mason-org/mason-lspconfig.nvim",-- {{{
+		opts = { ensure_installed = {"lua_ls"} },
+		dependencies = {
+			{ "mason-org/mason.nvim", opts = {} },
+			"neovim/nvim-lspconfig",
+		},-- }}}
+	},
+	{
+		'saghen/blink.cmp',-- {{{
+		version = '1.*',
+		opts = {
+			keymap = { preset = 'default' },
+			appearance = { nerd_font_variant = 'normal' },
+		},
+		sources = {
+			default = { 'lsp', 'path', 'snippets', 'buffer' },
+		},-- }}}
+	},
+	{
+		'windwp/nvim-autopairs',-- {{{
+		event = "InsertEnter",
+		config = true-- }}}
+	}
 }
