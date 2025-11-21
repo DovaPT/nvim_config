ad66cdb3d39bff7081d0832640265760f3b2f0a7
DovaPT <peterd5590@gmail.com>
2025-11-21 19:13:20 +0100

    updated treesitter and telescope to use main/master

--- init.lua
+++ init.lua
@@ -1,28 +1,32 @@
---Tab width and wrap{{{
+-- Tab width and wrap
 vim.o.shiftwidth = 4
 vim.o.tabstop = 4
 vim.o.expandtab = true
 vim.o.wrap = false
--- }}}
 
---Clipboard{{{
+-- Clipboard
 vim.o.clipboard = "unnamedplus"
--- }}}
 
---Folds{{{
-vim.o.foldmethod = 'marker'
--- }}}
+-- Folds
+vim.wo.foldmethod = 'expr'
+vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
 
---Lazy{{{
+-- Lazy
 require("config.lazy")
--- }}}
 
---Lsp{{{
+-- Lsp
 vim.lsp.inlay_hint.enable(true)
 vim.diagnostic.config({ virtual_lines = true, virtual_text = true })
--- }}}
+vim.lsp.config("rust-analyzer", {
+    rust_analyzer = {
+        check = {
+            command = "clippy",
+            extraArgs = "-- -W clippy::pedantinc"
+        }
+    }
+})
 
--- Themeing and coloring{{{
+-- Themeing and coloring
 vim.cmd.colorscheme("ayu-dark")
 vim.api.nvim_set_hl(0, 'LineNr', { fg = 'grey', bold = true })
 vim.api.nvim_set_hl(0, 'CursorLineNr', { fg = 'orange', bold = true })
@@ -33,8 +37,6 @@ vim.o.cursorline = true
 vim.o.signcolumn = "yes"
 vim.o.winborder = "single"
 vim.o.colorcolumn = "72"
--- }}}
 
---keybinds{{{
+-- Keybinds
 require("keybinds")
--- }}}
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,11 +1,11 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
-	branch = "master",
-  lazy = false,
-	opts = {
-		ensure_installed = {'rust', 'lua', 'python', 'zig'},
-	},
-	auto_install = true,
-	highlight = {enable = true,},
-  build = ":TSUpdate"
+    "nvim-treesitter/nvim-treesitter",
+    branch = "main",
+    lazy = false,
+    opts = {
+        ensure_installed = { 'rust', 'lua', 'python', 'zig' },
+    },
+    auto_install = true,
+    highlight = { enable = true, },
+    build = ":TSUpdate"
 }
